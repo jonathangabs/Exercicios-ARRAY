f8173e7c141be7b504d07cb52d5b9856f80a6343
Jonathan <endzs200@gmail.com>
2025-05-22 10:30:37 -0300

    Exercício 4 - Os dois maiores números e posições

---
+++ exercicio04.php
@@ -0,0 +1,23 @@
+<?php
+$vetor = [55,20,7,10,1];
+
+$maior1 = PHP_INT_MIN;
+$maior2 = PHP_INT_MIN;
+$pos1 = -1;
+$pos2 = -1;
+
+for ($i = 0; $i < count($vetor); $i++); {
+    if ($vetor[$i] > $maior1) {
+        $maior2 = $maior1;
+        $pos2 = $pos1;
+        $maior1 = $vetor[$i];
+        $pos1 = $i;
+    } elseif ($vetor[$i] > $maior2) {
+        $maior2 = $vetor[i];
+        $pos2 = $i;
+    }
+}
+
+echo "Primeiro maior: $maior1 (posição $pos1)\n";
+echo "Segundo maior: $maior2 (posição $pos2)\n";
+?>
