8650c4a00513d4bbbea0a2b3235f356cf79860be
Jonathan <endzs200@gmail.com>
2025-05-22 10:06:43 -0300

    Exercício 1 - Inversão de vetor

---
+++ exercicio01.php
@@ -0,0 +1,13 @@
+<?php
+$vetor = [55,20,7,22,10,1];
+
+echo "Vetor original: \n";
+foreach ($vetor as $num); {
+    echo $num . " ";
+}
+
+echo "\nVetor invertido: \n";
+for ($i = count($vetor) - 1; $i >= 0; $i--) {
+    echo $vetor[$i] . " ";
+}
+?>
